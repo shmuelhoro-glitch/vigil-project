480d96dfe05bdf83c68b3139ec27a8ef5845491b
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-07-06 17:48:41 +0300

    add fakeData and endpoint of get all. break evening

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import http from "http"
+import {getAll} from "./data/storage.js"
 
 const server = http.createServer((req, res) =>{
+    res.writeHead(200,{'content-type': 'application/json'})
     if (req.method === "GET" && req.url === '/health'){
         res.end("success")
+    }
+    else if (req.method === "GET" && req.url === "/heroes"){
+        getAll().then(data => res.end(JSON.stringify(data)))
     }
     else {res.end("err")}
 })
